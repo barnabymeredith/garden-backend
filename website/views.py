de28ffc6cf7aeacb7075b3621e4cb1190ff192d3
barnaby <barnabyhillis@gmail.com>
2023-04-10 15:31:19 +0100

    adds edit existing marker functionality

--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Marker
+from .serializers import MarkerSerializer
 
 from .models import Marker  
 from .serializers import MarkerSerializer
@@ -9,3 +13,15 @@ from rest_framework import viewsets
 class MarkerViewSet(viewsets.ModelViewSet):
     queryset = Marker.objects.all()
     serializer_class = MarkerSerializer
+
+    def update(self, request, pk=None):
+        try:
+            marker = Marker.objects.get(pk=pk)
+        except Marker.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.get_serializer(marker, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
